feffe4452b51fc12de8ffc884371eccada986680
Anders Skaalsveen <anders.skaalsveen@elate.no>
2024-01-22 09:57:20 +0100

    Fix lint issues and warnings

--- examples/print-barcode/src/main.rs
+++ examples/print-barcode/src/main.rs
@@ -1,11 +1,10 @@
-#![feature(slice_take)]
 extern crate brother_ql_rs;
 
 use barcoders::sym::ean13::EAN13;
-use brother_ql_rs::printer::constants::{MAX_PIXEL_WIDTH, RASTER_LINE_LENGTH};
+use brother_ql_rs::printer::constants::MAX_PIXEL_WIDTH;
 use brother_ql_rs::printer::job::PrintJob;
-use brother_ql_rs::printer::{Printable, printers, ThermalPrinter};
 use brother_ql_rs::printer::setting::Resolution;
+use brother_ql_rs::printer::{printers, Printable, ThermalPrinter};
 
 const BAR_HEIGHT: usize = 80;
 const BAR_WIDTH: usize = 4;
@@ -14,10 +13,16 @@ fn main() {
     let barcode = EAN13::new("7035620025037").unwrap();
     let encoded: Vec<u8> = barcode.encode();
 
-    let offset = (MAX_PIXEL_WIDTH - encoded.len() * BAR_WIDTH) / 2;
-    if offset < 0 {
-        panic!("The barcode exceeds the maximum image width.")
+    if BAR_WIDTH < 1 {
+        panic!("BAR_WIDTH must be greater than 0.")
     }
+
+    let barcode_bitmap_length = encoded.len();
+    if barcode_bitmap_length < 1 {
+        panic!("Failed to encode barcode.")
+    }
+
+    let offset = (MAX_PIXEL_WIDTH - encoded.len() * BAR_WIDTH) / 2;
 
     let mut row = [true; MAX_PIXEL_WIDTH];
     for (i, &b) in encoded.iter().enumerate() {
@@ -27,7 +32,7 @@ fn main() {
         }
     }
 
-    let line = vec![row.into_raster_line()];
+    let line = [row.into_raster_line()];
     let job = PrintJob {
         cut_on_end: true,
         raster_lines: line.repeat(BAR_HEIGHT),
@@ -41,7 +46,7 @@ fn main() {
                 println!("Sending job to printer...");
                 p.print(&job).unwrap()
             }
-            Err(e) => panic!("Failed to init Thermal Printer: {:?}", e)
+            Err(e) => panic!("Failed to init Thermal Printer: {:?}", e),
         };
     }
 }
